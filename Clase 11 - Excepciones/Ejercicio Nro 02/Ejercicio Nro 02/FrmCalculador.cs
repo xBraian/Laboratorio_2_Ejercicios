7b8222a6a2a65c6bc5a8487c1f379b314a62a8a4
xBraian <abraian2403@gmail.com>
2024-06-06 20:52:14 -0300

    Cambios ejercicio nro 02

--- Clase 11 - Excepciones/Ejercicio Nro 02/Ejercicio Nro 02/FrmCalculador.cs
+++ Clase 11 - Excepciones/Ejercicio Nro 02/Ejercicio Nro 02/FrmCalculador.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Entidades;
+using System;
 using System.Windows.Forms;
-using Entidades;
 
 namespace Ejercicio_Nro_02
 {
